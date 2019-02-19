f494d3354fca155877921c3a068363dc8f1aed4f
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-19 12:07:57 -0700

    Create ben.m

---
+++ 04_functions/ben.m
@@ -0,0 +1,51 @@
+% matlab functions and constants
+% by functions, there's mathematical functions such as sin and cos
+cos(0) % prints 1
+sin(0) % prints 0
+tan(0) % prints 1
+
+A = [1 2; 3 4]
+sin(A)
+
+sin(2)
+sin(3)
+sin(4)
+
+% exponential function 
+exp(0)
+
+% log which is oposite of exponential
+log(1)
+log(exp(1)) % prints 1
+
+sqrt(4) % prints 2
+
+pi % pi is a constant in Matlab
+
+% matlab doesn't have a variable, you have to do
+exp(1)
+
+sqrt(-1) % i is a constant
+
+i*i % prints -1
+
+sin(pi) * i*cos(pi)
+
+% create 3x3 identity matrix
+I = [1 0 0; 0 1 0; 0 0 1]
+
+eye(3) % does creates identity matrix
+
+zeros(3) % 3x3 matrix of all 0's
+
+zeros(3,2) % creates a 3,2 of all 0's
+
+ones(3,2) % creats a 3,2 of all ones
+
+my_sinh(1)
+
+x = 0:10;
+my_sinh(x) % hyperbolic sin for all numbers in x
+
+is_even([1 2 3 4 5])
+
