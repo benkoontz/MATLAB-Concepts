0102f883f2c44be253a080c8f59b17c06e4952b5
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-19 11:20:21 -0700

    Create ben.m

---
+++ 01_What is Matlab?/ben.m
@@ -0,0 +1,21 @@
+% what is MATLAB?
+
+% to get matlab you go to matlab's site
+
+% matlab stands for matrix laboratory
+
+% matlab is mostly for math
+
+% matlab is often used for analyzing data
+
+% matlab is used in engineering, finance, economics
+
+% matlab is kind of like a fancy calculator
+
+% you can plot data and implement algorithims
+
+% add ons are also called tool boxes and they have functionality
+% that can be used in your certain field of work
+
+% how to get help learning Matlab? it's usually best to google 
+% your questions
