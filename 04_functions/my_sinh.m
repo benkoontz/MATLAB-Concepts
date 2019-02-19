b75c0c0ab2ed19ccf8f4158539d48ca925e19771
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-19 12:08:20 -0700

    Create my_sinh.m

---
+++ 04_functions/my_sinh.m
@@ -0,0 +1,17 @@
+% function name has to be the same as the final name
+
+% function [outputArg] = untitled(inputArg)
+% %UNTITLED Summary of this function goes here
+% %   Detailed explanation goes here
+% outputArg1 = inputArg1;
+% outputArg2 = inputArg2;
+% end
+
+%hyperbolic sin function
+function y = my_sinh(x)
+y =(exp(x) - exp(-x))/2;
+
+end
+
+
+
